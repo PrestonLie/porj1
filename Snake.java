d3ec540da507dab580dc1ec52175e198b897a4ac
J <justinzhou210@gmail.com>
2022-05-25 20:34:34 -0700

    cracked commit

--- Snake.java
+++ Snake.java
@@ -1,83 +1,279 @@
-import java.util.Scanner;
 
-public class Snake {
+
+public class Snake 
+{
    private int length;
    private int speed;
    private String color;
    private int direction;
    private String directionString;
+   public String[][] terrain;
+   public int gridSize;
 
-   public Snake(int l, int s, String c, int d){
+   public Snake(int l, int s, String c, int d, int x, int y)
+   {
       length = l;
       speed = s;
       color = c;
       direction = d;
       updateDirectionString();
+      terrain = new String[y][x];
+      initializeTerrain();
+      gridSize = calcGridSize();
    }
 
-   public void updateDirectionString(){
-      if(direction%360 == 90){
-         directionString = "North";
-      } else if(direction%360 == 0){
-         directionString = "West";
-      } else if(direction%360 == 180){
-         directionString = "East";
-      } else if(direction%360 == 270){
-         directionString = "South";
+   public void initializeTerrain()
+   {
+      for(int i = 0; i < terrain.length; i++)
+      {
+         for(int j = 0; j < terrain[0].length; j++)
+         {
+            int rand = (int) (Math.random()*4);
+            if(rand == 0)
+            {
+               terrain[i][j] = "grassy";
+            }
+            else if(rand == 1)
+            {
+               terrain[i][j] = "marshy";
+            }
+            else if(rand == 2)
+            {
+               terrain[i][j] = "muddy";
+            }
+            else
+            {
+               terrain[i][j] = "sandy";
+            }
+         }
+      }
+      System.out.println("A " + terrain.length + " x " + terrain[0].length +" grid with 1 of 4 different terrain types at each coordinate has been created.");
+   }
+
+   public void canHide(int x, int y)
+   {
+
+      if(mainTerrainTypeOf(x, y).equalsIgnoreCase("grassy"))
+      {
+         if(color.equalsIgnoreCase("green"))
+         {
+            System.out.println("The terrain here is " + mainTerrainTypeOf(x, y));
+            System.out.println("The snake can hide on the coordinates (" + x + ", " + y + ") if the snake coils up.");
+         }
+         else
+         {
+            System.out.println("The snake cannot hide on the coordinates (" + x + ", " + y + ") since the snake is " + color + " and the terrain at the coordinates is mainly " + mainTerrainTypeOf(x, y));
+         }
+      }
+      else if(mainTerrainTypeOf(x, y).equalsIgnoreCase("marshy"))
+      {
+         if(color.equalsIgnoreCase("gray") || color.equalsIgnoreCase("grey"))
+         {
+            System.out.println("The terrain here is " + terrain[y][x]);
+            System.out.println("The snake can hide on the coordinates (" + x + ", " + y + ") if the snake coils up.");
+         }
+         else
+         {
+            System.out.println("The snake cannot hide on the coordinates (" + x + ", " + y + ") since the snake is " + color + " and the terrain at the coordinates is mainly " + mainTerrainTypeOf(x, y));
+         }
+      }
+      else if(mainTerrainTypeOf(x, y).equalsIgnoreCase("muddy"))
+      {
+         if(color.equalsIgnoreCase("brown"))
+         {
+            System.out.println("The terrain here is " + terrain[y][x]);
+            System.out.println("The snake can hide on the coordinates (" + x + ", " + y + ") if the snake coils up.");
+         }
+         else
+         {
+            System.out.println("The snake cannot hide on the coordinates (" + x + ", " + y + ") since the snake is " + color + " and the terrain at the coordinates is mainly " + mainTerrainTypeOf(x, y));
+         }
+      }
+      else if(mainTerrainTypeOf(x, y).equalsIgnoreCase("sandy"))
+      {
+         if(color.equalsIgnoreCase("yellow") || color.equalsIgnoreCase("orange"))
+         {
+            System.out.println("The terrain here is " + terrain[y][x]);
+            System.out.println("The snake can hide on the coordinates (" + x + ", " + y + ") if the snake coils up.");
+         }
+         else
+         {
+            System.out.println("The snake cannot hide on the coordinates (" + x + ", " + y + ") since the snake is " + color + " and the terrain at the coordinates is mainly " + mainTerrainTypeOf(x, y));
+         }
+      }
+      else if(mainTerrainTypeOf(x, y).equalsIgnoreCase("mixed"))
+      {
+         System.out.println("The snake cannot hide on the coordinates (" + x + ", " + y + ") since the snake is " + color + " and the terrain at the coordinates is mainly " + mainTerrainTypeOf(x, y));
+      }
+      else
+      {
+         System.out.println("The x or y values selected do not fit on the terrain grid.");
       }
    }
+   
 
-   public String prompt(String promptText){
+   public String mainTerrainTypeOf(int x, int y){
 
-      Scanner sc = new Scanner(System.in);
-      System.out.println(promptText);
-      return sc.nextLine();
+      int sandCount = 0;
+      int marshCount = 0;
+      int grassCount = 0;
+      int mudCount = 0;
+      try
+      {
+         for(int i = y - gridSize/2; i <= y + gridSize/2; i++)
+         {
+            for(int j = x - gridSize/2; j <= x + gridSize/2; j++)
+            {
+               if(terrain[i][j].equals("marshy"))
+               {
+                  marshCount++;
+               }
+               else if(terrain[i][j].equals("sandy"))
+               {
+                  sandCount++;
+               }
+               else if(terrain[i][j].equals("muddy"))
+               {
+                  mudCount++;
+               }
+               else if(terrain[i][j].equals("grassy"))
+               {
+                  grassCount++;
+               }
+            }
+         }
+      
+         if(findMax(marshCount, sandCount, mudCount, grassCount) > 4 && marshCount == findMax(marshCount, sandCount, mudCount, grassCount))
+         {
+            return "marshy";
+         } 
+         else if(findMax(marshCount, sandCount, mudCount, grassCount) > 4 && sandCount == findMax(marshCount, sandCount, mudCount, grassCount))
+         {
+            return "sandy";
+         }
+         else if(findMax(marshCount, sandCount, mudCount, grassCount) > 4 && mudCount == findMax(marshCount, sandCount, mudCount, grassCount))
+         {
+            return "muddy";
+         }
+         else if(findMax(marshCount, sandCount, mudCount, grassCount) > 4 && grassCount == findMax(marshCount, sandCount, mudCount, grassCount))
+         {
+            return "grassy";
+         }
+         else
+         {
+            return "mixed";
+         }
+      }  
+      catch(Exception e)
+      {
+         return "error";
+      }
 
+      
    }
    
-   public void travel(int xFrom, int yFrom, int xTo, int yTo){
+   public int calcGridSize(){
+      int count = 0;
+      for(int i = length; i > 0; i/=10){
+         count ++;
+      }
+      return count*2 - 1;
+   }
+
+   public int findMax(int a, int b, int c, int d){
+      int max = a;
+      max = Math.max(max, b);
+      max = Math.max(max, c);
+      max = Math.max(max, d);
+      return max;
+   }
+
+   public void updateDirectionString()
+   {
+      if(direction%360 == 90)
+      {
+         directionString = "north";
+      } 
+      else if(direction%360 == 0)
+      {
+         directionString = "west";
+      } 
+      else if(direction%360 == 180)
+      {
+         directionString = "east";
+      } 
+      else if(direction%360 == 270)
+      {
+         directionString = "south";
+      }
+   }
+   
+   public void travel(int xFrom, int yFrom, int xTo, int yTo)
+   {
 
       String directionX;
       String directionY;
 
-      if(xFrom-xTo > 0){
+      if(xFrom-xTo > 0)
+      {
          directionX = "west";
          direction = 180;
          updateDirectionString();
-      } else if(xFrom-xTo < 0) {
+      } 
+      else if(xFrom-xTo < 0) 
+      {
          directionX = "east";
          direction = 0;
          updateDirectionString();
-      } else{
+      } 
+      else
+      {
          directionX = "";
       }
 
-      if(yFrom-yTo > 0){
+      if(yFrom-yTo > 0)
+      {
          directionY = "south";
          direction = 270;
          updateDirectionString();
-      } else if(yFrom-yTo < 0) {
+      } 
+      else if(yFrom-yTo < 0) 
+      {
          directionY = "north";
          direction = 90;
          updateDirectionString();
-      } else{
+      } 
+      else
+      {
          directionY = "";
       }
 
-      if(!directionX.equals("") && !directionY.equals("")){
+      if(!directionX.equals("") && !directionY.equals(""))
+      {
          System.out.println("The snake moves toward the " + directionX + " for " + (double)(Math.abs(xFrom-xTo))/speed + " seconds, covering " + Math.abs(xFrom-xTo) + " feet.");
          System.out.println("The snake moves toward the " + directionY + " for " + (double)(Math.abs(yFrom-yTo))/speed + " seconds, covering " + Math.abs(yFrom-yTo) + " feet.");
-      }else if(directionX.equals("") && !directionY.equals("")){
+      }
+      else if(directionX.equals("") && !directionY.equals(""))
+      {
          System.out.println("The snake moves toward the " + directionY + " for " + (double)(Math.abs(yFrom-yTo))/speed + " seconds, covering " + Math.abs(yFrom-yTo) + " feet.");
-      }else if(!directionX.equals("") && directionY.equals("")){
+      }
+      else if(!directionX.equals("") && directionY.equals(""))
+      {
          System.out.println("The snake moves toward the " + directionX + " for " + (double)(Math.abs(xFrom-xTo))/speed + " seconds, covering " + Math.abs(xFrom-xTo) + " feet.");
-      }else{
+      }
+      else
+      {
          System.out.println("The snake stays still, facing " + directionString + ".");
       }
+
+      System.out.println("The snake is currently facing " + directionString + ".");
    }
 
-   public String toString(){
+   public String toString()
+   {
+
       return "This snake is " + length + " feet long, goes at " + speed + " feet per second, is " + color + ", and is heading toward the " + directionString + ".";
+
    }
 
    public void setDirection(int d)
@@ -100,6 +296,10 @@ public class Snake {
       length = l;
    }
 
+   public String getTerrainType(int x, int y)
+   {
+      return terrain[y][x];
+   }
    public String getColor()
    {
       return color;
@@ -120,33 +320,33 @@ public class Snake {
       return speed;
    }
 
-   public void forward(double time)
-   {
-      System.out.println("Snake has gone" + speed * time + "feet forward.");
-      updateDirectionString();
-      System.out.println("The snake is facing " + directionString + ".");
-   }
-   public void back(double time)
-   {
-      System.out.println("Snake has gone" + speed * time + "feet backward.");
-      direction -= 180;
-      updateDirectionString();
-      System.out.println("The snake is facing " + directionString + ".");
-   }
-   public void right(int time)
-   {
-      System.out.println("Snake has gone" + speed * time + "feet to the right");
-      direction -= 90;
-      updateDirectionString();
-      System.out.println("The snake is facing " + directionString + ".");
-   }
-   public void left(int time)
-   {
-      System.out.println("Snake has gone" + speed * time + "feet to the left");
-      direction += 90;
-      updateDirectionString();
-      System.out.println("The snake is facing " + directionString + ".");
-   }
+   // public void forward(double time)
+   // {
+   //    System.out.println("Snake has gone" + speed * time + "feet forward.");
+   //    updateDirectionString();
+   //    System.out.println("The snake is facing " + directionString + ".");
+   // }
+   // public void back(double time)
+   // {
+   //    System.out.println("Snake has gone" + speed * time + "feet backward.");
+   //    direction -= 180;
+   //    updateDirectionString();
+   //    System.out.println("The snake is facing " + directionString + ".");
+   // }
+   // public void right(int time)
+   // {
+   //    System.out.println("Snake has gone" + speed * time + "feet to the right");
+   //    direction -= 90;
+   //    updateDirectionString();
+   //    System.out.println("The snake is facing " + directionString + ".");
+   // }
+   // public void left(int time)
+   // {
+   //    System.out.println("Snake has gone" + speed * time + "feet to the left");
+   //    direction += 90;
+   //    updateDirectionString();
+   //    System.out.println("The snake is facing " + directionString + ".");
+   // }
    
 
 }
--- SnakeRunner.java
+++ SnakeRunner.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+import java.lang.Integer;
+
 public class SnakeRunner{
 
     public static void main(String args[]){
+        System.out.println("Welcome! This is a program meant to represent a snake and its properties. The snake object has 4 initial attributes: length, speed, color, and initial direction. \nAdditionally, a grid representing a map of different terrain types will be randomly generated, and this grid has dimensions that can be set.");
+        System.out.println("Without further ado, lets set the attributes of our new snake object.");
 
-        Snake mySnake = new Snake(80, 50, "green", 90);
+        Snake mySnake = new Snake(Integer.parseInt(prompt("Please enter the integer length of the snake, in feet:")), Integer.parseInt(prompt("Please enter the integer speed of the snake, in feet per second:")), prompt("Please enter the color of the snake:"), Integer.parseInt(prompt("Please enter the integer direction of the snake, with 90 being North and 270 being South:")), Integer.parseInt(prompt("Please enter the width of the terrain grid:")), Integer.parseInt(prompt("Please enter the height of the terrain grid:")));
+        System.out.println("The following method will be a determination of whether or not our custom lengthed snake can hide at specific coordinates on the terrain grid. \nIf the snake's length is 1 digit long, only the terrain of at the base coordinates are checked. If the snake's length is 2 digits long, the 3x3 grid around the coordinates will be checked for a terrain majority. \nIf the snake's length is 3 digits long, the 5x5 grid will be checked, so on and so forth. This is done to reflect the fact that longer snakes take up more space when coiled up.");
+        mySnake.canHide(Integer.parseInt(prompt("Please enter the integer x coordinate: ")), Integer.parseInt(prompt("Please enter the integer y coordinate: ")));
 
-        mySnake.travel(0,0,100,100);
+
+        System.out.println("Now let's begin testing the travel method of the snake class. This method takes in the x and y values of the starting coordinates and the ending coordinates and narrates the snake's trip from the starting point to the ending point.");
+        mySnake.travel(Integer.parseInt(prompt("Please enter the integer starting x coordinate: ")),Integer.parseInt(prompt("Please enter the integer starting y coordinate: ")),Integer.parseInt(prompt("Please enter the integer ending x coordinate: ")),Integer.parseInt(prompt("Please enter the integer ending y coordinate: ")));
+    }
+
+    public static String prompt(String promptText)
+    {
+
+      Scanner sc = new Scanner(System.in);
+      System.out.println(promptText);
+      return sc.nextLine();
+
     }
 }
